55376d87298188f2575311fe596954bd9008b3d8
Theodore Ts'o <tytso@mit.edu>
1999-07-31 00:32:57 +0000

    ChangeLog, debugfs.c:   debugfs.c (dump_inode): Fix debugfs message so it is the same for when   	compiled under either Linux 2.2 or 2.3.  This allows for the f_swapfs   	regression test suite to work regardless of which OS e2fsprogs was   	compiled on.

--- debugfs/debugfs.c
+++ debugfs/debugfs.c
@@ -307,10 +307,10 @@ static void dump_inode(ino_t inode_num, struct ext2_inode inode)
 	else i_type = "bad type";
 	fprintf(out, "Inode: %ld   Type: %s    ", inode_num, i_type);
 #if defined(HAVE_EXT2_INODE_VERSION)
-	fprintf(out, "Mode:  %04o   Flags: 0x%x   Version: %d\n",
+	fprintf(out, "Mode:  %04o   Flags: 0x%x   Version/Generation: %d\n",
 		inode.i_mode & 0777, inode.i_flags, inode.i_version);
 #else
-	fprintf(out, "Mode:  %04o   Flags: 0x%x   Generation: %d\n",
+	fprintf(out, "Mode:  %04o   Flags: 0x%x   Version/Generation: %d\n",
 		inode.i_mode & 0777, inode.i_flags, inode.i_generation);
 #endif
 	fprintf(out, "User: %5d   Group: %5d   Size: ",
