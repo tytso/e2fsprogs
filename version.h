776485657d9753e41d1e6b74aa5920696cc2d0c0
Theodore Ts'o <tytso@mit.edu>
2001-05-14 13:04:44 +0000

    RELEASE-NOTES:   Update a typo and updated date for next WIP release. e2fsprogs.lsm:   Updated approximate values for 1.20 release version.h:   Update for new WIP release.

--- version.h
+++ version.h
@@ -2,9 +2,9 @@
  * version.h --- controls the version number printed by the e2fs
  * programs.
  * 
- * Copyright 1995, 1996, 1997, 1998, 1999 by Theodore Ts'o.  This file
- * may be redistributed under the GNU Public License.
+ * Copyright 1995, 1996, 1997, 1998, 1999, 2000, 2001 by Theodore
+ * Ts'o.  This file may be redistributed under the GNU Public License.
  */
 
 #define E2FSPROGS_VERSION "1.20-WIP"
-#define E2FSPROGS_DATE "10-May-2001"
+#define E2FSPROGS_DATE "14-May-2001"
