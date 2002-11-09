38513011b951e60abe25d85f10eec53f1fa73b7b
Theodore Ts'o <tytso@mit.edu>
2002-11-09 15:00:56 -0500

    Update files for 1.32 release.

--- version.h
+++ version.h
@@ -6,5 +6,5 @@
  * Ts'o.  This file may be redistributed under the GNU Public License.
  */
 
-#define E2FSPROGS_VERSION "1.31"
-#define E2FSPROGS_DATE "08-Nov-2002"
+#define E2FSPROGS_VERSION "1.32"
+#define E2FSPROGS_DATE "09-Nov-2002"
