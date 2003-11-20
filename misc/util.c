d07b150371f4a46f2729ab2635fc8f23c9ffca58
Theodore Ts'o <tytso@mit.edu>
2003-11-20 18:34:20 -0500

    util.c (check_plausibility): Support 2.6 kernel header files, 	instead of having the build to crash.  (Addresses Debian 	bug #221778)

--- misc/util.c
+++ misc/util.c
@@ -109,8 +109,20 @@ void check_plausibility(const char *device)
 #define MINOR(dev)	((dev) & 0xff)
 #endif
 #ifndef SCSI_BLK_MAJOR
+#ifdef SCSI_DISK0_MAJOR
+#ifdef SCSI_DISK8_MAJOR
+#define SCSI_DISK_MAJOR(M) ((M) == SCSI_DISK0_MAJOR || \
+  ((M) >= SCSI_DISK1_MAJOR && (M) <= SCSI_DISK7_MAJOR) || \
+  ((M) >= SCSI_DISK8_MAJOR && (M) <= SCSI_DISK15_MAJOR))
+#else
+#define SCSI_DISK_MAJOR(M) ((M) == SCSI_DISK0_MAJOR || \
+  ((M) >= SCSI_DISK1_MAJOR && (M) <= SCSI_DISK7_MAJOR))
+#endif /* defined(SCSI_DISK8_MAJOR) */
+#define SCSI_BLK_MAJOR(M) (SCSI_DISK_MAJOR((M)) || (M) == SCSI_CDROM_MAJOR)
+#else
 #define SCSI_BLK_MAJOR(M)  ((M) == SCSI_DISK_MAJOR || (M) == SCSI_CDROM_MAJOR)
-#endif
+#endif /* defined(SCSI_DISK0_MAJOR) */
+#endif /* defined(SCSI_BLK_MAJOR) */
 	if (((MAJOR(s.st_rdev) == HD_MAJOR &&
 	      MINOR(s.st_rdev)%64 == 0) ||
 	     (SCSI_BLK_MAJOR(MAJOR(s.st_rdev)) &&
