feb1219f7953a3bbd0b49ae81db418fccd83993d
Theodore Ts'o <tytso@mit.edu>
2005-06-20 18:00:55 -0400

    Update for e2fsprogs 1.38-WIP-0620 release.

--- version.h
+++ version.h
@@ -2,9 +2,10 @@
  * version.h --- controls the version number printed by the e2fs
  * programs.
  * 
- * Copyright 1995, 1996, 1997, 1998, 1999, 2000, 2001 by Theodore
- * Ts'o.  This file may be redistributed under the GNU Public License.
+ * Copyright 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
+ * 2004, 2005 by Theodore Ts'o.  This file may be redistributed under
+ * the GNU Public License.
  */
 
 #define E2FSPROGS_VERSION "1.38-WIP"
-#define E2FSPROGS_DATE "09-May-2005"
+#define E2FSPROGS_DATE "20-Jun-2005"
