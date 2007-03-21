2f1ecfa5f8ca0e03fc7684962443829b7f20c7d1
Brian Behlendorf <behlendorf1@llnl.gov>
2007-03-21 19:16:11 -0400

    [COVERITY] Fix memory leak on error handling in the debugfs's icheck command
    Coverity ID: 25: Resource Leak
    
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- debugfs/icheck.c
+++ debugfs/icheck.c
@@ -86,7 +86,7 @@ void do_icheck(int argc, char **argv)
 
 	for (i=1; i < argc; i++) {
 		if (strtoblk(argv[0], argv[i], &bw.barray[i-1].blk))
-			return;
+			goto error_out;
 	}
 
 	bw.num_blocks = bw.blocks_left = argc-1;
@@ -160,7 +160,8 @@ void do_icheck(int argc, char **argv)
 
 error_out:
 	free(bw.barray);
-	free(block_buf);
+	if (block_buf)
+		free(block_buf);
 	if (scan)
 		ext2fs_close_inode_scan(scan);
 	return;
