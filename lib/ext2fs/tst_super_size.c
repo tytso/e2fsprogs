a2b2ff61c88ac7583f288a1723d8195a0b7f2f83
Theodore Ts'o <tytso@mit.edu>
2007-05-31 12:39:02 -0400

    Add test to make sure the ext2 superblock structure is 1024 bytes
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

---
+++ lib/ext2fs/tst_super_size.c
@@ -0,0 +1,28 @@
+/*
+ * This testing program makes sure superblock size is 1024 bytes long
+ *
+ * Copyright (C) 2007 by Theodore Ts'o.
+ * 
+ * %Begin-Header%
+ * This file may be redistributed under the terms of the GNU Public
+ * License.
+ * %End-Header%
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "ext2_fs.h"
+
+int main(int argc, char **argv)
+{
+	int s = sizeof(struct ext2_super_block);
+
+	printf("Size of struct ext2_super_block is %d\n", s);
+	if (s != 1024) {
+		exit(1);
+	}
+	exit(0);
+}
+		
