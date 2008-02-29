e1e83b6ba9c14e6f9a845c5a9694e2c74dffef0a
Theodore Ts'o <tytso@mit.edu>
2008-02-28 22:51:49 -0500

    Update release notes, version files for 1.40.7 release
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * redistributed under the GNU Public License.
  */
 
-#define E2FSPROGS_VERSION "1.40.6"
-#define E2FSPROGS_DATE "09-Feb-2008"
+#define E2FSPROGS_VERSION "1.40.7"
+#define E2FSPROGS_DATE "28-Feb-2008"
