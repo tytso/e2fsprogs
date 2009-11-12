06ef971be505678ee462ae1844204ed24f14aedc
Theodore Ts'o <tytso@mit.edu>
2009-11-12 17:54:45 -0500

    libss: initialize function pointer for readline_shutdown()
    Clear the function pointer for readline_shutdown() so that if libss is
    linking against a readline library which doesn't supply a
    readline_shutdown() function, ss_delete_invocation() won't seg fault.
    
    Thanks to Solar Designer <solar@openwall.com> for reporting this
    problem to me.
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- lib/ss/invocation.c
+++ lib/ss/invocation.c
@@ -71,6 +71,7 @@ int ss_create_invocation(subsystem_name, version_string, info_ptr,
 	*(new_table->rqt_tables+1) = (ss_request_table *) NULL;
 
 	new_table->readline_handle = 0;
+	new_table->readline_shutdown = 0;
 	new_table->readline = 0;
 	new_table->add_history = 0;
 	new_table->redisplay = 0;
