5ab348723247f5f7f55adae74e3f5f7c2fc103e7
Theodore Ts'o <tytso@mit.edu>
2012-02-17 22:58:30 -0500

    Fix missing version.h update
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.42"
-#define E2FSPROGS_DATE "29-Nov-2011"
+#define E2FSPROGS_VERSION "1.42.1"
+#define E2FSPROGS_DATE "17-Feb-2012"
