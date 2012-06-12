d6a3e41172ce43b4a92fae0791beb7592a4575b1
Theodore Ts'o <tytso@mit.edu>
2012-06-12 17:21:38 -0400

    Update Release Notes, Changelogs, version.h, etc. for 1.42.4 release
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.42.3"
-#define E2FSPROGS_DATE "14-May-2012"
+#define E2FSPROGS_VERSION "1.42.4"
+#define E2FSPROGS_DATE "12-June-2012"
