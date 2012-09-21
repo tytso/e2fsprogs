11e5292266b26c63c6ffd25b799d271088200256
Theodore Ts'o <tytso@mit.edu>
2012-09-21 12:53:14 -0400

    Update Release Notes, Changelogs, version.h, etc. for 1.42.6 release

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.42.5"
-#define E2FSPROGS_DATE "29-Jul-2012"
+#define E2FSPROGS_VERSION "1.42.6"
+#define E2FSPROGS_DATE "21-Sep-2012"
