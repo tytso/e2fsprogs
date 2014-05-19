9779e296103959026309265be5dcfe735a2f33d4
Theodore Ts'o <tytso@mit.edu>
2014-05-18 21:51:21 -0400

    Update release notes, etc. for final 1.42.10 release
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.42.9"
-#define E2FSPROGS_DATE "4-Feb-2014"
+#define E2FSPROGS_VERSION "1.42.10"
+#define E2FSPROGS_DATE "18-May-2014"
