4ac4a41b8b0dbcd9159b0a2449b5dc307d0ec77a
Theodore Ts'o <tytso@mit.edu>
2015-07-12 16:56:39 -0400

    android_config: remove HAVE_TERMIO_H
    Sync up with aosp's e2fsprogs commits:
    
        dc351db427e667b9f954c4338f69da83fa16222b
        2ca3c65991609bb5b48004ec3c7a66f6b417beed
        4ef951aa5e69116963f1836fe5ddcf6b87afbecd
    
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- util/android_config.h
+++ util/android_config.h
@@ -35,7 +35,6 @@
 #define HAVE_SYS_TIME_H 1
 #define HAVE_SYS_TYPES_H 1
 #define HAVE_SYS_WAIT_H 1
-#define HAVE_TERMIO_H 1
 #define HAVE_TYPE_SSIZE_T 1
 #define HAVE_UNISTD_H 1
 #define HAVE_UTIME_H 1
