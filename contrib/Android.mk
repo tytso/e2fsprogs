b97bccecfe97270eb15f77d1580f16eb1125fc1c
Theodore Ts'o <tytso@mit.edu>
2015-07-14 23:19:08 -0400

    android: add add_ext4_encrypt helper program
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- contrib/Android.mk
+++ contrib/Android.mk
@@ -34,3 +34,46 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_HOST_EXECUTABLE)
 
+#########################################################################
+# Build add_ext4_encrypt
+#
+include $(CLEAR_VARS)
+
+add_ext4_encrypt_src_files := \
+	add_ext4_encrypt.c
+
+add_ext4_encrypt_c_includes := \
+	external/e2fsprogs/lib
+
+add_ext4_encrypt_cflags := -O2 -g -W -Wall
+
+add_ext4_encrypt_shared_libraries := \
+	libext2fs \
+	libext2_com_err
+
+add_ext4_encrypt_system_shared_libraries := libc
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(add_ext4_encrypt_src_files)
+LOCAL_C_INCLUDES := $(add_ext4_encrypt_c_includes)
+LOCAL_CFLAGS := $(add_ext4_encrypt_cflags)
+LOCAL_SHARED_LIBRARIES := $(add_ext4_encrypt_shared_libraries)
+LOCAL_SYSTEM_SHARED_LIBRARIES := $(add_ext4_encrypt_system_shared_libraries)
+LOCAL_MODULE := add_ext4_encrypt
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(add_ext4_encrypt_src_files)
+LOCAL_C_INCLUDES := $(add_ext4_encrypt_c_includes)
+LOCAL_CFLAGS := $(add_ext4_encrypt_cflags)
+LOCAL_SHARED_LIBRARIES := $(addsuffix _host, $(add_ext4_encrypt_shared_libraries))
+LOCAL_MODULE := add_ext4_encrypt_host
+LOCAL_MODULE_STEM := add_ext4_encrypt
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_HOST_EXECUTABLE)
+
