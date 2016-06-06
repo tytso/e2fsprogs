be05f60ecfea19a257035765377dc662bcd67565
Theodore Ts'o <tytso@mit.edu>
2016-06-06 16:49:37 -0400

    tests,debugfs: ea_set's "-f <value_file" needs to be before the other args
    For systems that don't use GNU's getopt(3), options have to be before
    all non-option arguments.  So change the usage message for debugfs's
    ea_set command, and then fix the d_xattr_sorting test.
    
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- debugfs/xattrs.c
+++ debugfs/xattrs.c
@@ -196,15 +196,14 @@ void do_set_xattr(int argc, char **argv)
 			}
 			break;
 		default:
-			printf("%s: Usage: %s <file> <attr> [-f infile | "
-			       "value]\n", argv[0], argv[0]);
-			goto out2;
+			goto print_usage;
 		}
 	}
 
 	if (!(fp && optind == argc - 2) && !(!fp && optind == argc - 3)) {
-		printf("%s: Usage: %s <file> <attr> [-f infile | value>]\n",
-		       argv[0], argv[0]);
+	print_usage:
+		printf("Usage:\t%s <file> <attr> <value>\n", argv[0]);
+		printf("\t%s -f <value_file> <file> <attr>\n", argv[0]);
 		goto out2;
 	}
 
