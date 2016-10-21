cab4931cd0db6e87fed4bfe119a55991e547e4da
Jeff Sharkey <jsharkey@android.com>
2016-10-21 12:44:14 -0600

    Bring back libext2_profile build rules.
    Google-Bug-Id: 27599744
    From AOSP commit: 80fcd5bb6dcc130337def60423a5d9e22a5814e7
    
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- lib/support/Android.mk
+++ lib/support/Android.mk
@@ -55,3 +55,41 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := $(addsuffix -host, $(libext2_quota_shared_libraries))
 
 include $(BUILD_HOST_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+libext2_profile_src_files :=  \
+       prof_err.c \
+       profile.c
+
+libext2_profile_shared_libraries := \
+       libext2_com_err
+
+libext2_profile_system_shared_libraries := libc
+
+libext2_profile_c_includes := external/e2fsprogs/lib
+
+libext2_profile_cflags := -O2 -g -W -Wall
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(libext2_profile_src_files)
+LOCAL_SYSTEM_SHARED_LIBRARIES := $(libext2_profile_system_shared_libraries)
+LOCAL_SHARED_LIBRARIES := $(libext2_profile_shared_libraries)
+LOCAL_C_INCLUDES := $(libext2_profile_c_includes)
+LOCAL_CFLAGS := $(libext2_profile_cflags)
+LOCAL_MODULE := libext2_profile
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(libext2_profile_src_files)
+LOCAL_SHARED_LIBRARIES := $(addsuffix -host, $(libext2_profile_shared_libraries))
+LOCAL_C_INCLUDES := $(libext2_profile_c_includes)
+LOCAL_CFLAGS := $(libext2_profile_cflags)
+LOCAL_MODULE := libext2_profile-host
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_HOST_SHARED_LIBRARY)
