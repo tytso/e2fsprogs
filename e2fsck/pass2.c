a06b42383c8633ad7880cce5025c7941174a8909
Theodore Ts'o <tytso@mit.edu>
2017-04-14 16:44:16 -0400

    e2fsck: fix type mismatch bug in the largedir patch
    Commit ae9efd05a986: "e2fsck: 3 level hash tree directory
    optimization" introduced a bug when checking for the largedir feature.
    
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- e2fsck/pass2.c
+++ e2fsck/pass2.c
@@ -978,7 +978,7 @@ static int check_dir_block(ext2_filsys fs,
 	 * very large and then the files are deleted. For now, all that is
 	 * needed is to avoid e2fsck filling in these holes as part of
 	 * feature flag. */
-	if (db->blk == 0 && ext2fs_has_feature_largedir(fs))
+	if (db->blk == 0 && ext2fs_has_feature_largedir(fs->super))
 		return 0;
 
 	if (db->blk == 0 && !inline_data_size) {
