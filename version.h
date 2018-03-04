c8c41d480311e1c0b5b2d587a3ea0799974216fa
Theodore Ts'o <tytso@mit.edu>
2018-03-04 16:30:46 -0500

    Update release notes, etc., for the 1.44.0-rc2 release
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.44.0-rc1"
-#define E2FSPROGS_DATE "27-Feb-2018"
+#define E2FSPROGS_VERSION "1.44.0-rc2"
+#define E2FSPROGS_DATE "4-Mar-2018"
