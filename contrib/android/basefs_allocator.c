269f4c1bc7ca2ecf1d2926d8153488570f8b266c
David Anderson <dvander@google.com>
2019-12-04 17:38:11 -0800

    AOSP: e2fsdroid: Refactor block_range.
    block_range is a singly-linked list, but the head/tail links are
    manually managed all over. Instead, introduce a block_range_list
    structure and refactor list helpers to operate on this instead. This
    ensures head/tail are maintained properly (in some cases, like
    delete_block_range, they were not).
    
    Bug: 145316683
    Test: manual test
    Change-Id: Ieec6324549e2c3a71129871f703f4f0a37aeb1fa
    From AOSP commit: 4220594792297619d2e70a29476667d1698dbd63

--- contrib/android/basefs_allocator.c
+++ contrib/android/basefs_allocator.c
@@ -13,8 +13,11 @@ struct base_fs_allocator {
 static errcode_t basefs_block_allocator(ext2_filsys, blk64_t, blk64_t *,
 					struct blk_alloc_ctx *ctx);
 
-static void fs_free_blocks_range(ext2_filsys fs, struct block_range *blocks)
+static void fs_free_blocks_range(ext2_filsys fs,
+				 struct block_range_list *list)
 {
+	struct block_range *blocks = list->head;
+
 	while (blocks) {
 		ext2fs_unmark_block_bitmap_range2(fs->block_map, blocks->start,
 			blocks->end - blocks->start + 1);
@@ -22,8 +25,10 @@ static void fs_free_blocks_range(ext2_filsys fs, struct block_range *blocks)
 	}
 }
 
-static void fs_reserve_blocks_range(ext2_filsys fs, struct block_range *blocks)
+static void fs_reserve_blocks_range(ext2_filsys fs,
+				    struct block_range_list *list)
 {
+	struct block_range *blocks = list->head;
 	while (blocks) {
 		ext2fs_mark_block_bitmap_range2(fs->block_map,
 			blocks->start, blocks->end - blocks->start + 1);
@@ -50,7 +55,7 @@ errcode_t base_fs_alloc_load(ext2_filsys fs, const char *file,
 	if (retval)
 		goto err_bitmap;
 	while ((e = ext2fs_hashmap_iter_in_order(entries, &it)))
-		fs_reserve_blocks_range(fs, e->head);
+		fs_reserve_blocks_range(fs, &e->blocks);
 
 	allocator->cur_entry = NULL;
 	allocator->entries = entries;
@@ -72,19 +77,12 @@ static errcode_t basefs_block_allocator(ext2_filsys fs, blk64_t goal,
 					blk64_t *ret, struct blk_alloc_ctx *ctx)
 {
 	errcode_t retval;
-	struct block_range *next_range;
 	struct base_fs_allocator *allocator = fs->priv_data;
 	struct basefs_entry *e = allocator->cur_entry;
 
 	/* Try to get a block from the base_fs */
-	if (e && e->head && ctx && (ctx->flags & BLOCK_ALLOC_DATA)) {
-		*ret = e->head->start;
-		e->head->start += 1;
-		if (e->head->start > e->head->end) {
-			next_range = e->head->next;
-			free(e->head);
-			e->head = next_range;
-		}
+	if (e && e->blocks.head && ctx && (ctx->flags & BLOCK_ALLOC_DATA)) {
+		*ret = consume_next_block(&e->blocks);
 	} else { /* Allocate a new block */
 		retval = ext2fs_new_block2(fs, goal, fs->block_map, ret);
 		if (retval)
@@ -101,9 +99,8 @@ void base_fs_alloc_cleanup(ext2_filsys fs)
 	struct base_fs_allocator *allocator = fs->priv_data;
 
 	while ((e = ext2fs_hashmap_iter_in_order(allocator->entries, &it))) {
-		fs_free_blocks_range(fs, e->head);
-		delete_block_ranges(e->head);
-		e->head = e->tail = NULL;
+		fs_free_blocks_range(fs, &e->blocks);
+		delete_block_ranges(&e->blocks);
 	}
 
 	fs->priv_data = NULL;
@@ -140,9 +137,7 @@ errcode_t base_fs_alloc_unset_target(ext2_filsys fs,
 	if (!allocator || !allocator->cur_entry || mode != S_IFREG)
 		return 0;
 
-	fs_free_blocks_range(fs, allocator->cur_entry->head);
-	delete_block_ranges(allocator->cur_entry->head);
-	allocator->cur_entry->head = allocator->cur_entry->tail = NULL;
-	allocator->cur_entry = NULL;
+	fs_free_blocks_range(fs, &allocator->cur_entry->blocks);
+	delete_block_ranges(&allocator->cur_entry->blocks);
 	return 0;
 }
