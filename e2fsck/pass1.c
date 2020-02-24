c36fa1b28bd4171f53ab4f89a90f6b3b8bf80fd2
Wang Shilong <wshilong@ddn.com>
2020-02-24 13:08:58 +0800

    e2fsck: merge context flags properly
    e2fsck might restart after pass1, so we should keep
    flags if possible, this patch try to fix f_illitable_flexbg failure
    
    Signed-off-by: Wang Shilong <wshilong@ddn.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Signed-off-by: Saranya Muruganandam <saranyamohan@google.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- e2fsck/pass1.c
+++ e2fsck/pass1.c
@@ -2573,9 +2573,7 @@ static int e2fsck_pass1_thread_join_one(e2fsck_t global_ctx, e2fsck_t thread_ctx
 	global_ctx->fs_fragmented_dir += fs_fragmented_dir;
 	global_ctx->large_files += large_files;
 
-	/* Keep the global singal flags*/
-	global_ctx->flags |= (flags & E2F_FLAG_SIGNAL_MASK) |
-			     (global_ctx->flags & E2F_FLAG_SIGNAL_MASK);
+	global_ctx->flags |= flags;
 
 	retval = e2fsck_pass1_merge_fs(global_fs, thread_fs);
 	if (retval) {
