29d22c467547f11f4db31331549ee4e304eeb220
Theodore Ts'o <tytso@mit.edu>
2020-03-20 23:51:07 -0400

    Update release notes, etc., for the 1.45.6 release
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.45.5"
-#define E2FSPROGS_DATE "07-Jan-2020"
+#define E2FSPROGS_VERSION "1.45.6"
+#define E2FSPROGS_DATE "20-Mar-2020"
