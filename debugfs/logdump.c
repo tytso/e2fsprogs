tytso/e2fsprogs

commit bd9ac48c9200bbfb9964d31e33c6aacb26ce49ff

debugfs: add journal header checks in logdump
Addresses-Coverity-Bug: 1431466
Addresses-Coverity-Bug: 1432478
Signed-off-by: Theodore Ts'o <tytso@mit.edu>

debugfs/logdump.c
@@ -406,7 +406,12 @@ static void dump_journal(char *cmdname, FILE *out_file,
 			"Journal superblock magic number invalid!\n");
 		return;
 	}
-	blocksize = be32_to_cpu(jsb->s_blocksize);
+	if (be32_to_cpu(jsb->s_blocksize) != blocksize) {
+		fprintf(out_file,
+			"Journal block size invalid: %u\n",
+			be32_to_cpu(jsb->s_blocksize));
+		return;
+	}
 	transaction = be32_to_cpu(jsb->s_sequence);
 	blocknr = be32_to_cpu(jsb->s_start);
 
@@ -690,6 +695,11 @@ static void dump_revoke_block(FILE *out_file, char *buf,
 	header = (jbd2_journal_revoke_header_t *) buf;
 	offset = sizeof(jbd2_journal_revoke_header_t);
 	max = be32_to_cpu(header->r_count);
+	if (max > jsb->s_blocksize) {
+		fprintf(out_file, "Revoke block's r_count invalid: %u\b",
+			max);
+		max = jsb->s_blocksize;
+	}
 
 	while (offset < max) {
 		if (tag_size == sizeof(__u32)) {

commit eef68a593f7f1a1c60be6da670cc3c7abb7d013a

debugfs: fix header checks in logdump
Fixes: bd9ac48c9200 ("debugfs: add journal header checks in logdump")
Signed-off-by: Theodore Ts'o <tytso@mit.edu>

debugfs/logdump.c
@@ -406,10 +406,13 @@ static void dump_journal(char *cmdname, FILE *out_file,
 			"Journal superblock magic number invalid!\n");
 		return;
 	}
-	if (be32_to_cpu(jsb->s_blocksize) != blocksize) {
+	blocksize = be32_to_cpu(jsb->s_blocksize);
+	if ((current_fs && (blocksize != current_fs->blocksize)) ||
+	    (!current_fs && (!blocksize || (blocksize & (blocksize - 1)) ||
+			     (blocksize > 65536)))) {
 		fprintf(out_file,
-			"Journal block size invalid: %u\n",
-			be32_to_cpu(jsb->s_blocksize));
+			"Journal block size invalid: %u (%u)\n",
+			be32_to_cpu(jsb->s_blocksize), blocksize);
 		return;
 	}
 	transaction = be32_to_cpu(jsb->s_sequence);