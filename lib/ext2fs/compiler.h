a39e58bab602dc39cf263954c4484c8d12bf306c
Michael Forney <mforney@mforney.org>
2021-04-14 00:41:28 -0700

    libext2fs: use offsetof() from stddef.h
    offsetof is a standard C feature available from stddef.h, going
    back all the way to ANSI C.
    
    Signed-off-by: Michael Forney <mforney@mforney.org>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- lib/ext2fs/compiler.h
+++ lib/ext2fs/compiler.h
@@ -1,18 +1,7 @@
 #ifndef _EXT2FS_COMPILER_H
 #define _EXT2FS_COMPILER_H
 
-#ifndef __has_builtin
-#define __has_builtin(x) 0
-#endif
-
-#undef offsetof
-#if __has_builtin(__builtin_offsetof)
-#define offsetof(TYPE, MEMBER) __builtin_offsetof(TYPE, MEMBER)
-#elif defined(__compiler_offsetof)
-#define offsetof(TYPE,MEMBER) __compiler_offsetof(TYPE,MEMBER)
-#else
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
-#endif
+#include <stddef.h>
 
 #ifdef __GNUC__
 #define container_of(ptr, type, member) ({			\
