496669a2900e6be58dcf200b8ea1802a866ef3b8
Theodore Ts'o <tytso@mit.edu>
2021-07-27 12:46:39 -0400

    Update release notes, etc., for the 1.46.3 release
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.46.2"
-#define E2FSPROGS_DATE "28-Feb-2021"
+#define E2FSPROGS_VERSION "1.46.3"
+#define E2FSPROGS_DATE "27-Jul-2021"
