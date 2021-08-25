a8f525888f608d6966e49637ed62c88887177532
Jan Kara <jack@suse.cz>
2021-08-26 00:11:34 +0200

    dumpe2fs, debugfs, e2image: Add support for orphan file
    Print inode number of orphan file in outputs, dump e2image file to
    filesystem image.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- debugfs/set_fields.c
+++ debugfs/set_fields.c
@@ -183,6 +183,7 @@ static struct field_set_info super_fields[] = {
 	{ "lpf_ino", &set_sb.s_lpf_ino, NULL, 4, parse_uint },
 	{ "checksum_seed", &set_sb.s_checksum_seed, NULL, 4, parse_uint },
 	{ "encoding", &set_sb.s_encoding, NULL, 2, parse_encoding },
+	{ "orphan_file_inum", &set_sb.s_orphan_file_inum, NULL, 4, parse_uint },
 	{ 0, 0, 0, 0 }
 };
 
