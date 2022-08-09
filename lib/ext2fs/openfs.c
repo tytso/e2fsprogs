42c11edd0863e093a2cb84c56720af0e9b8e8c68
Theodore Ts'o <tytso@mit.edu>
2022-08-08 20:17:40 -0400

    libext2fs: in ext2fs_open[2](), return an error if s_desc_size is too large
    Previously, ext2fs_open() and ext2fs_open2() would return an error if
    s_desc_size is too small.  Add a check so it will return an error if
    s_desc_size is too large, as well.
    
    These checks will be skipped for e2fsck when it uses the flag
    EXT2_FLAG_IGNORE_SB_ERRORS.
    
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- lib/ext2fs/openfs.c
+++ lib/ext2fs/openfs.c
@@ -330,8 +330,13 @@ retry:
 	}
 
 	/* Enforce the block group descriptor size */
-	if (ext2fs_has_feature_64bit(fs->super)) {
-		if (fs->super->s_desc_size < EXT2_MIN_DESC_SIZE_64BIT) {
+	if (!(flags & EXT2_FLAG_IGNORE_SB_ERRORS) &&
+	    ext2fs_has_feature_64bit(fs->super)) {
+		unsigned desc_size = fs->super->s_desc_size;
+
+		if ((desc_size < EXT2_MIN_DESC_SIZE_64BIT) ||
+		    (desc_size > EXT2_MAX_DESC_SIZE) ||
+		    (desc_size & (desc_size - 1)) != 0) {
 			retval = EXT2_ET_BAD_DESC_SIZE;
 			goto cleanup;
 		}
