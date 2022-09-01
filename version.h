58a366d1339c09497e3a92a9bdacdf3b79b905d2
Theodore Ts'o <tytso@mit.edu>
2022-09-01 10:56:20 -0400

    Start bumping version numbers in preparation for 1.46.6 release
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.46.5"
-#define E2FSPROGS_DATE "30-Dec-2021"
+#define E2FSPROGS_VERSION "1.46.6-rc0"
+#define E2FSPROGS_DATE "1-Sep-2022"
