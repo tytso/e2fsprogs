281dcbfb1d59d0db95d4f81b8b9c275d87515ceb
Theodore Ts'o <tytso@mit.edu>
2022-09-13 07:59:52 -0400

    libext2fs: in ext2fs_open[2](), return an error if s_first_meta_bg is too big
    These checks will be skipped for e2fsck when it uses the flag
    EXT2_FLAG_IGNORE_SB_ERRORS.
    
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- lib/ext2fs/openfs.c
+++ lib/ext2fs/openfs.c
@@ -409,6 +409,12 @@ retry:
 	}
 	fs->desc_blocks = ext2fs_div_ceil(fs->group_desc_count,
 					  EXT2_DESC_PER_BLOCK(fs->super));
+	if (ext2fs_has_feature_meta_bg(fs->super) &&
+	    (fs->super->s_first_meta_bg > fs->desc_blocks) &&
+	    !(flags & EXT2_FLAG_IGNORE_SB_ERRORS)) {
+		retval = EXT2_ET_CORRUPT_SUPERBLOCK;
+		goto cleanup;
+	}
 	if (flags & EXT2_FLAG_SUPER_ONLY)
 		goto skip_read_bg;
 	retval = ext2fs_get_array(fs->desc_blocks, fs->blocksize,
