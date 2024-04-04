tytso/e2fsprogs

commit 42c11edd0863e093a2cb84c56720af0e9b8e8c68

libext2fs: in ext2fs_open[2](), return an error if s_desc_size is too large
Previously, ext2fs_open() and ext2fs_open2() would return an error if
s_desc_size is too small.  Add a check so it will return an error if
s_desc_size is too large, as well.

These checks will be skipped for e2fsck when it uses the flag
EXT2_FLAG_IGNORE_SB_ERRORS.

Signed-off-by: Theodore Ts'o <tytso@mit.edu>

lib/ext2fs/openfs.c
@@ -330,8 +330,13 @@ retry:
 	}
 
 	/* Enforce the block group descriptor size */
-	if (ext2fs_has_feature_64bit(fs->super)) {
-		if (fs->super->s_desc_size < EXT2_MIN_DESC_SIZE_64BIT) {
+	if (!(flags & EXT2_FLAG_IGNORE_SB_ERRORS) &&
+	    ext2fs_has_feature_64bit(fs->super)) {
+		unsigned desc_size = fs->super->s_desc_size;
+
+		if ((desc_size < EXT2_MIN_DESC_SIZE_64BIT) ||
+		    (desc_size > EXT2_MAX_DESC_SIZE) ||
+		    (desc_size & (desc_size - 1)) != 0) {
 			retval = EXT2_ET_BAD_DESC_SIZE;
 			goto cleanup;
 		}

tests/f_desc_size_bad/expect.1
@@ -1,3 +1,5 @@
+../e2fsck/e2fsck: Block group descriptor size incorrect while trying to open test.img
+../e2fsck/e2fsck: Trying to load superblock despite errors...
 ext2fs_check_desc: Block group descriptor size incorrect
 ../e2fsck/e2fsck: Group descriptors look bad... trying backup blocks...
 Pass 1: Checking inodes, blocks, and sizes

e2fsck/unix.c
@@ -1619,7 +1619,8 @@ failure:
 			 * so that we are able to recover from more errors
 			 * (e.g. some tool messing up some value in the sb).
 			 */
-			if ((retval == EXT2_ET_CORRUPT_SUPERBLOCK) &&
+			if (((retval == EXT2_ET_CORRUPT_SUPERBLOCK) ||
+			     (retval == EXT2_ET_BAD_DESC_SIZE)) &&
 			    !(flags & EXT2_FLAG_IGNORE_SB_ERRORS)) {
 				if (fs)
 					ext2fs_close_free(&fs);

commit ef4e0825eee3d720a2df762249f0e80bb5f3ef3b

libext2fs: always refuse to open a file system with a zero s_desc_size
Commit 42c11edd0863 ("ext2fs_open[2](), return an error if s_desc_size
is too large") added a check for an insanely large s_desc_size to
prevent some failures triggered by fuzz testing.  However, it would
allow e2fsck to fall back to recover the file system by using the
backup superblocks by having e2fsck pass the flag
EXT2_FLAG_IGNORE_SB_ERRORS.  But by allowing an s_desc_Size of zero,
it's possible that e2fsck will die with a division of zero error.
With this fix, e2fsck will now print an error message and exit
instead.

https://github.com/tytso/e2fsprogs/issues/183

Fixes: 42c11edd0863 ("ext2fs_open[2](), return an error if s_desc_size is too large")
Signed-off-by: Theodore Ts'o <tytso@mit.edu>

lib/ext2fs/openfs.c
@@ -330,13 +330,14 @@ retry:
 	}
 
 	/* Enforce the block group descriptor size */
-	if (!(flags & EXT2_FLAG_IGNORE_SB_ERRORS) &&
-	    ext2fs_has_feature_64bit(fs->super)) {
+	if (ext2fs_has_feature_64bit(fs->super)) {
 		unsigned desc_size = fs->super->s_desc_size;
 
-		if ((desc_size < EXT2_MIN_DESC_SIZE_64BIT) ||
-		    (desc_size > EXT2_MAX_DESC_SIZE) ||
-		    (desc_size & (desc_size - 1)) != 0) {
+		if (desc_size == 0 ||
+		    (!(flags & EXT2_FLAG_IGNORE_SB_ERRORS) &&
+		     ((desc_size > EXT2_MAX_DESC_SIZE) ||
+		      (desc_size < EXT2_MIN_DESC_SIZE_64BIT) ||
+		      (desc_size & (desc_size - 1)) != 0))) {
 			retval = EXT2_ET_BAD_DESC_SIZE;
 			goto cleanup;
 		}

tests/f_desc_size_zero/expect.1
@@ -1,0 +1,13 @@
+../e2fsck/e2fsck: Block group descriptor size incorrect while trying to open test.img
+../e2fsck/e2fsck: Trying to load superblock despite errors...
+../e2fsck/e2fsck: Block group descriptor size incorrect while trying to open test.img
+
+The superblock could not be read or does not describe a valid ext2/ext3/ext4
+filesystem.  If the device is valid and it really contains an ext2/ext3/ext4
+filesystem (and not swap or ufs or something else), then the superblock
+is corrupt, and you might try running e2fsck with an alternate superblock:
+    e2fsck -b 8193 <device>
+ or
+    e2fsck -b 32768 <device>
+
+Exit status is 8

tests/f_desc_size_zero/name
@@ -1,0 +1,1 @@
+zero s_desc_size

tests/f_desc_size_zero/script
@@ -1,0 +1,2 @@
+ONE_PASS_ONLY="true"
+. $cmd_dir/run_e2fsck