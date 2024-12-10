tytso/e2fsprogs

commit daf2d1c830d9fb778001493e76f820c6c00d22dd

libext2fs: fix ext2fs_orphan_file_block_csum_set()
The function ext2fs_orphan_file_block_csum_set() isn't used by
e2fsprogs, but it was embarassingly broken.  Thanks to Darrick Wong
for pointing this out.

Fixes: 16534ff59444 ("e2fsck: fix big-endian support for orphan_file...")
Signed-off-by: Theodore Ts'o <tytso@mit.edu>

lib/ext2fs/orphan.c
@@ -250,14 +250,16 @@ errcode_t ext2fs_orphan_file_block_csum_set(ext2_filsys fs, ext2_ino_t ino,
 					    blk64_t blk, char *buf)
 {
 	struct ext4_orphan_block_tail *tail;
+	errcode_t ret;
 	__u32 crc;
 
 	if (!ext2fs_has_feature_metadata_csum(fs->super))
 		return 0;
 
 	tail = ext2fs_orphan_block_tail(fs, buf);
-	return ext2fs_orphan_file_block_csum(fs, ino, blk, buf, &crc);
+	ret = ext2fs_orphan_file_block_csum(fs, ino, blk, buf, &crc);
 	tail->ob_checksum = ext2fs_cpu_to_le32(crc);
+	return ret;
 }
 
 int ext2fs_orphan_file_block_csum_verify(ext2_filsys fs, ext2_ino_t ino,

commit c1e0fa3231d3639c79f4460bc9e38d6c91cafa6c

libext2fs: fix compiler warning in ext2fs_orphan_file_block_csum_set()
Fixes: daf2d1c830d9 ("libext2fs: fix ext2fs_orphan_file_block_csum_set()")
Signed-off-by: Theodore Ts'o <tytso@mit.edu>

lib/ext2fs/orphan.c
@@ -251,13 +251,15 @@ errcode_t ext2fs_orphan_file_block_csum_set(ext2_filsys fs, ext2_ino_t ino,
 {
 	struct ext4_orphan_block_tail *tail;
 	errcode_t ret;
-	__u32 crc;
+	__u32 crc = 0;
 
 	if (!ext2fs_has_feature_metadata_csum(fs->super))
 		return 0;
 
 	tail = ext2fs_orphan_block_tail(fs, buf);
 	ret = ext2fs_orphan_file_block_csum(fs, ino, blk, buf, &crc);
+	if (ret)
+		return 0;
 	tail->ob_checksum = ext2fs_cpu_to_le32(crc);
 	return ret;
 }