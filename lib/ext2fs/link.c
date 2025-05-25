tytso/e2fsprogs

commit 53aa6c54224f545756da312e8756576dbaabe08a

libext2fs: add the EXT2FS_LINK_APPEND flag to ext2fs_link()
Add a flag which only tries to add the new directory entry to the last
block in the directory.  This is helpful avoids mke2fs -d offering
from an O(n**2) performance bottleneck when adding a large number of
files to a directory.

Signed-off-by: Theodore Ts'o <tytso@mit.edu>

lib/ext2fs/link.c
@@ -254,7 +254,8 @@ static int link_proc(ext2_ino_t dir EXT2FS_ATTR((unused)),
 	ext2fs_dirent_set_name_len(dirent, ls->namelen);
 	strncpy(dirent->name, ls->name, ls->namelen);
 	if (ext2fs_has_feature_filetype(ls->sb))
-		ext2fs_dirent_set_file_type(dirent, ls->flags & 0x7);
+		ext2fs_dirent_set_file_type(dirent,
+					    ls->flags & EXT2FS_LINK_FT_MASK);
 
 	ls->done++;
 	return DIRENT_ABORT|DIRENT_CHANGED;
@@ -633,8 +634,34 @@ errcode_t ext2fs_link(ext2_filsys fs, ext2_ino_t dir, const char *name,
 	ls.blocksize = fs->blocksize;
 	ls.err = 0;
 
-	retval = ext2fs_dir_iterate2(fs, dir, DIRENT_FLAG_INCLUDE_EMPTY,
-				     NULL, link_proc, &ls);
+	if ((flags & EXT2FS_LINK_APPEND) &&
+	    !(inode.i_flags & EXT4_INLINE_DATA_FL)) {
+		blk64_t		lblk, pblk = 0;
+		struct dir_context ctx;
+
+		lblk = (inode.i_size / fs->blocksize) - 1;
+		retval = ext2fs_bmap2(fs, ino, &inode, NULL, 0, lblk,
+				      NULL, &pblk);
+		if (retval)
+			return retval;
+
+		ctx.dir = dir;
+		ctx.flags = flags;
+		ctx.func = link_proc;
+		ctx.priv_data = &ls;
+		ctx.errcode = 0;
+		retval = ext2fs_get_mem(fs->blocksize, &ctx.buf);
+		if (retval)
+			return retval;
+
+		ext2fs_process_dir_block(fs, &pblk, lblk, 0, 0, &ctx);
+		retval = ctx.errcode;
+		ext2fs_free_mem(&ctx.buf);
+	} else {
+		retval = ext2fs_dir_iterate2(fs, dir,
+					     DIRENT_FLAG_INCLUDE_EMPTY,
+					     NULL, link_proc, &ls);
+	}
 	if (retval)
 		return retval;
 	if (ls.err)

lib/ext2fs/ext2fs.h
@@ -795,6 +795,20 @@ struct ext2_xattr_handle;
 #define EXT2FS_BITMAPS_VALID_FLAGS	0x0007
 
 /*
+ * flags for ext2fs_unlink()
+ */
+
+/* Forcefully unlink even if the inode number doesn't match the dirent */
+#define EXT2FS_UNLINK_FORCE		0x1
+
+/*
+ * flags for ext2fs_link()
+ *
+ */
+#define EXT2FS_LINK_FT_MASK	0x0007
+#define EXT2FS_LINK_APPEND	0x0010
+
+/*
  * function prototypes
  */
 static inline int ext2fs_has_group_desc_csum(ext2_filsys fs)
@@ -1816,10 +1830,6 @@ extern errcode_t ext2fs_get_pathname(ext2_filsys fs, ext2_ino_t dir, ext2_ino_t 
 			       char **name);
 
 /* link.c */
-#define EXT2FS_UNLINK_FORCE		0x1	/* Forcefully unlink even if
-						 * the inode number doesn't
-						 * match the dirent
-						 */
 errcode_t ext2fs_link(ext2_filsys fs, ext2_ino_t dir, const char *name,
 		      ext2_ino_t ino, int flags);
 errcode_t ext2fs_unlink(ext2_filsys fs, ext2_ino_t dir, const char *name,

commit aef4ee829aa6318ce812ae8fad7425c21dd4220c

libext2fs: fix ext2fs_link() for EXT2FS_LINK_APPEND and non-regular files
Fix the incorrect flag being passed to ext2fs_process_dir_block().
This bug was masked because EXT2_FT_REG_FILE has the same code point
as DIRENT_FLAG_INCLUDE_EMPTY which was the flag that was needed and
mke2fs -d was only use ext2fs_lik() for regular files.

Fixes: 53aa6c54224f ("libext2fs: add the EXT2FS_LINK_APPEND flag ...)
Signed-off-by: Theodore Ts'o <tytso@mit.edu>

lib/ext2fs/link.c
@@ -647,7 +647,7 @@ retry:
 			return retval;
 
 		ctx.dir = dir;
-		ctx.flags = flags;
+		ctx.flags = DIRENT_FLAG_INCLUDE_EMPTY;
 		ctx.func = link_proc;
 		ctx.priv_data = &ls;
 		ctx.errcode = 0;