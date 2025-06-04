tytso/e2fsprogs

commit 5c7fec6121e6b24732db30ac408d9c8ab4805245

fuse2fs: support XATTR_CREATE/REPLACE in setxattr
Fix the setxattr implementation to support the create and replace flags
instead of performing an upsert regardless of inputs.

Cc: linux-ext4@vger.kernel.org # v1.43
Fixes: 81cbf1ef4f5dab ("misc: add fuse2fs, a FUSE server for e2fsprogs")
Signed-off-by: Darrick J. Wong <djwong@kernel.org>
Link: https://lore.kernel.org/r/174786677657.1383760.12232239908991346763.stgit@frogsfrogsfrogs
Signed-off-by: Theodore Ts'o <tytso@mit.edu>

misc/fuse2fs.c
@@ -18,6 +18,9 @@
 # include <linux/falloc.h>
 # include <linux/xattr.h>
 #endif
+#ifdef HAVE_SYS_XATTR_H
+#include <sys/xattr.h>
+#endif
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <ctype.h>
@@ -2517,7 +2520,7 @@ out:
 
 static int op_setxattr(const char *path EXT2FS_ATTR((unused)),
 		       const char *key, const char *value,
-		       size_t len, int flags EXT2FS_ATTR((unused)))
+		       size_t len, int flags)
 {
 	struct fuse_context *ctxt = fuse_get_context();
 	struct fuse2fs *ff = (struct fuse2fs *)ctxt->private_data;
@@ -2526,6 +2529,9 @@ static int op_setxattr(const char *path EXT2FS_ATTR((unused)),
 	ext2_ino_t ino;
 	errcode_t err;
 	int ret = 0;
+
+	if (flags & ~(XATTR_CREATE | XATTR_REPLACE))
+		return -EOPNOTSUPP;
 
 	FUSE2FS_CHECK_CONTEXT(ff);
 	fs = ff->fs;
@@ -2559,6 +2565,31 @@ static int op_setxattr(const char *path EXT2FS_ATTR((unused)),
 	if (err) {
 		ret = translate_error(fs, ino, err);
 		goto out2;
+	}
+
+	if (flags & (XATTR_CREATE | XATTR_REPLACE)) {
+		void *buf;
+		size_t buflen;
+
+		err = ext2fs_xattr_get(h, key, &buf, &buflen);
+		switch (err) {
+		case EXT2_ET_EA_KEY_NOT_FOUND:
+			if (flags & XATTR_REPLACE) {
+				ret = -ENODATA;
+				goto out2;
+			}
+			break;
+		case 0:
+			ext2fs_free_mem(&buf);
+			if (flags & XATTR_CREATE) {
+				ret = -EEXIST;
+				goto out2;
+			}
+			break;
+		default:
+			ret = translate_error(fs, ino, err);
+			goto out2;
+		}
 	}
 
 	err = ext2fs_xattr_set(h, key, value, len);

commit 9882047bee29ded1c5c642a63166973cab29fb04

misc: define XATTR_{CREATE,REMOVE} if necessary
This fixes a FreeBSD portability issue in fuse2fs.

Fixes: 5c7fec6121e6 ("fuse2fs: support XATTR_CREATE/REPLACE in setxattr")
Signed-off-by: Theodore Ts'o <tytso@mit.edu>

misc/fuse2fs.c
@@ -90,6 +90,20 @@
 #define XATTR_SECURITY_PREFIX_LEN (sizeof (XATTR_SECURITY_PREFIX) - 1)
 #endif
 
+/*
+ * Linux and MacOS implement the setxattr(2) interface, which defines
+ * XATTR_CREATE and XATTR_REPLACE.  However, FreeBSD uses
+ * extattr_set_file(2), which does not have a flags or options
+ * parameter, and does not define XATTR_CREATE and XATTR_REPLACE.
+ */
+#ifndef XATTR_CREATE
+#define XATTR_CREATE 0
+#endif
+
+#ifndef XATTR_REPLACE
+#define XATTR_REPLACE 0
+#endif
+
 #if !defined(EUCLEAN)
 #if !defined(EBADMSG)
 #define EUCLEAN EBADMSG