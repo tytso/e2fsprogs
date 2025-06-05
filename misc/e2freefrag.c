tytso/e2fsprogs

commit 0c675a67c5684252e3a228c824b0accb9f3ab5d7

debugfs: return after printing the usage message in the e2freefrag command
Signed-off-by: Theodore Ts'o <tytso@mit.edu>

misc/e2freefrag.c
@@ -397,6 +397,9 @@ int main(int argc, char *argv[])
 				fprintf(stderr, "%s: bad chunk size '%s'\n",
 					progname, optarg);
 				usage(progname);
+#ifdef DEBUFS
+				return;
+#endif
 			}
 			if (chunk_info.chunkbytes &
 			    (chunk_info.chunkbytes - 1)) {
@@ -409,6 +412,9 @@ int main(int argc, char *argv[])
 		case 'h':
 		default:
 			usage(progname);
+#ifdef DEBUGFS
+			return;
+#endif
 			break;
 		}
 	}
@@ -417,6 +423,9 @@ int main(int argc, char *argv[])
 	if (optind == argc) {
 		fprintf(stderr, "%s: missing device name.\n", progname);
 		usage(progname);
+#ifdef DEBUGFS
+		return;
+#endif
 	}
 
 	device_name = argv[optind];

commit 030b523d84a96c941cf9914444e39254aff10e59

e2freefrag: correct a mistyped symbol name
Fixes: 0c675a67c568 ("debugfs: return after printing the usage message..."(
Signed-off-by: Benno Schulenberg <bensberg@telfort.nl>
Signed-off-by: Theodore Ts'o <tytso@mit.edu>

misc/e2freefrag.c
@@ -397,7 +397,7 @@ int main(int argc, char *argv[])
 				fprintf(stderr, "%s: bad chunk size '%s'\n",
 					progname, optarg);
 				usage(progname);
-#ifdef DEBUFS
+#ifdef DEBUGFS
 				return;
 #endif
 			}