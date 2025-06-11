459efa346874a11b4911809e75d6eee157792ca5
Darrick J. Wong <djwong@kernel.org>
2025-06-11 09:43:45 -0700

    libext2fs: fix spurious warnings from fallocate
    generic/522 routinely produces error messages from fuse2fs like this:
    
    FUSE2FS (sde): Illegal block number passed to ext2fs_test_block_bitmap #9321 for block bitmap for /dev/sde
    
    Curiously, these don't actually result in errors being thrown up to the
    kernel.  Digging into the program (which was more difficult than it
    needed to be because of the weird bitmap base + errcode weirdness)
    produced a left record:
    
    e_lblk = 16
    e_pblk = 9293
    e_len = 6
    e_flags = 0
    
    and a right record:
    
    e_lblk = 45
    e_pblk = 9321
    e_len = 6
    e_flags = 0
    
    Thus we end up in the "Merge both extents together, perhaps?" section of
    ext_falloc_helper.  Unfortunately, the merge selection code isn't smart
    enough to notice that the two mappings aren't actually physically
    contiguous, so it scans the bitmap with a negative length, which is why
    the assertion trips.
    
    The simple fix here is not to try to merge the adjacent extents if
    they're not actually physically contiguous.
    
    Cc: linux-ext4@vger.kernel.org # v1.43
    Fixes: 5aad5b8e0e3cfa ("libext2fs: implement fallocate")
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- lib/ext2fs/fallocate.c
+++ lib/ext2fs/fallocate.c
@@ -276,6 +276,11 @@ try_merge:
 				max_uninit_len : max_init_len))
 			goto try_left;
 
+		/* Would they even be physically contiguous if merged? */
+		if (left_ext->e_pblk + left_ext->e_len + range_len !=
+				right_ext->e_pblk)
+			goto try_left;
+
 		err = ext2fs_extent_goto(handle, left_ext->e_lblk);
 		if (err)
 			goto try_left;
