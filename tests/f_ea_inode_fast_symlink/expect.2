tytso/e2fsprogs

commit 0684a4f33b5c268fe12f57fcbc77a880c79ab282

Overhaul extended attribute handling.  Should now be correct with respect to the latest V2 bestbits ACL code.

lib/ext2fs/valid_blk.c
@@ -38,8 +38,19 @@ int ext2fs_inode_has_valid_blocks(struct ext2_inode *inode)
 	 * If the symbolic link is a "fast symlink", then the symlink
 	 * target is stored in the block entries.
 	 */
-	if (LINUX_S_ISLNK (inode->i_mode) && inode->i_blocks == 0)
-		return 0;
-
+	if (LINUX_S_ISLNK (inode->i_mode)) {
+		if (inode->i_file_acl == 0) {
+			/* With no EA block, we can rely on i_blocks */
+			if (inode->i_blocks == 0)
+				return 0;
+		} else {
+			/* With an EA block, life gets more tricky */
+			if (inode->i_size >= EXT2_N_BLOCKS*4)
+				return 1; /* definitely using i_block[] */
+			if (inode->i_size > 3 && inode->i_block[1] != 0)
+				return 1; /* probably using i_block[] */
+			return 0; /* Probably a fast symlink */
+		}
+	}
 	return 1;
 }

tests/f_badinode/expect.1
@@ -1,6 +1,8 @@
 Filesystem did not have a UUID; generating one.
 
 Pass 1: Checking inodes, blocks, and sizes
+Inode 12, i_blocks is 2, should be 0.  Fix? yes
+
 Pass 2: Checking directory structure
 Inode 12 (/motd) has a bad mode (0110444).
 Clear? yes
@@ -30,12 +32,6 @@ Free blocks count wrong for group #0 (76, counted=77).
 Fix? yes
 
 Free blocks count wrong (76, counted=77).
-Fix? yes
-
-Free inodes count wrong for group #0 (16, counted=20).
-Fix? yes
-
-Free inodes count wrong (16, counted=20).
 Fix? yes
 
 

tests/f_badroot/expect.1
@@ -23,21 +23,6 @@ Connect to /lost+found? yes
 Inode 12 ref count is 2, should be 1.  Fix? yes
 
 Pass 5: Checking group summary information
-Free blocks count wrong for group #0 (77, counted=76).
-Fix? yes
-
-Free blocks count wrong (77, counted=76).
-Fix? yes
-
-Free inodes count wrong for group #0 (20, counted=19).
-Fix? yes
-
-Directories count wrong for group #0 (2, counted=3).
-Fix? yes
-
-Free inodes count wrong (20, counted=19).
-Fix? yes
-
 
 test_filesys: ***** FILE SYSTEM WAS MODIFIED *****
 test_filesys: 13/32 files (0.0% non-contiguous), 24/100 blocks

tests/f_badsymlinks/expect.1
@@ -1,6 +1,4 @@
 Pass 1: Checking inodes, blocks, and sizes
-Inode 16, i_size is 4294967358, should be 1024.  Fix? yes
-
 Special (device/socket/fifo/symlink) file (inode 18) has immutable
 or append-only flag set.  Clear? yes
 
@@ -51,16 +49,10 @@ Clear? yes
 Pass 3: Checking directory connectivity
 Pass 4: Checking reference counts
 Pass 5: Checking group summary information
-Free blocks count wrong for group #0 (995, counted=1001).
+Free blocks count wrong for group #0 (1000, counted=1001).
 Fix? yes
 
-Free blocks count wrong (995, counted=1001).
-Fix? yes
-
-Free inodes count wrong for group #0 (9, counted=19).
-Fix? yes
-
-Free inodes count wrong (9, counted=19).
+Free blocks count wrong (1000, counted=1001).
 Fix? yes
 
 

tests/f_badtable/expect.1
@@ -21,19 +21,22 @@ Pass 5: Checking group summary information
 Block bitmap differences:  -(12--20)
 Fix? yes
 
-Free blocks count wrong for group #0 (78, counted=87).
+Free blocks count wrong for group #0 (77, counted=87).
 Fix? yes
 
-Free blocks count wrong (78, counted=87).
+Free blocks count wrong (77, counted=87).
 Fix? yes
 
 Inode bitmap differences:  +(12--16) +(25--32)
 Fix? yes
 
-Free inodes count wrong for group #0 (21, counted=7).
+Free inodes count wrong for group #0 (20, counted=7).
 Fix? yes
 
-Free inodes count wrong (21, counted=7).
+Directories count wrong for group #0 (3, counted=2).
+Fix? yes
+
+Free inodes count wrong (20, counted=7).
 Fix? yes
 
 

tests/f_dupdot/expect.1
@@ -16,12 +16,6 @@ Inode 13 ref count is 2, should be 1.  Fix? yes
 Unattached zero-length inode 14.  Clear? yes
 
 Pass 5: Checking group summary information
-Free inodes count wrong for group #0 (2, counted=3).
-Fix? yes
-
-Free inodes count wrong (2, counted=3).
-Fix? yes
-
 
 test_filesys: ***** FILE SYSTEM WAS MODIFIED *****
 test_filesys: 13/16 files (0.0% non-contiguous), 22/100 blocks

tests/f_filetype/expect.1
@@ -37,6 +37,12 @@ Setting filetype for entry '..' in /dir (13) to 2.
 Pass 3: Checking directory connectivity
 Pass 4: Checking reference counts
 Pass 5: Checking group summary information
+Free inodes count wrong for group #0 (50, counted=47).
+Fix? yes
+
+Free inodes count wrong (50, counted=47).
+Fix? yes
+
 
 test_filesys: ***** FILE SYSTEM WAS MODIFIED *****
 test_filesys: 17/64 files (0.0% non-contiguous), 27/100 blocks

tests/f_illitable/expect.1
@@ -28,10 +28,19 @@ Pass 5: Checking group summary information
 Block bitmap differences:  -(11--21)
 Fix? yes
 
-Free blocks count wrong for group #0 (78, counted=89).
+Free blocks count wrong for group #0 (77, counted=89).
 Fix? yes
 
-Free blocks count wrong (78, counted=89).
+Free blocks count wrong (77, counted=89).
+Fix? yes
+
+Free inodes count wrong for group #0 (20, counted=21).
+Fix? yes
+
+Directories count wrong for group #0 (3, counted=2).
+Fix? yes
+
+Free inodes count wrong (20, counted=21).
 Fix? yes
 
 

commit ae7370a5fa175c609bd7c780a5ffaefdd58efc91

ext2fs: fix fast symlink blocks check
Use ext4_inode_is_fast_symlink() in ext2fs_inode_has_valid_blocks2()
instead of depending exclusively on i_blocks == 0 to determine
if an inode is a fast symlink. Otherwise, if a fast symlink has a
large external xattr inode that increases i_blocks, it will be
incorrectly reported as having invalid blocks.

Change-Id: Ibde2348da39401601abedd603bd7e4ef97091abe
Fixes: 0684a4f33 ("Overhaul extended attribute handling")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Li Dongyang <dongyangli@ddn.com>
Reviewed-on: https://review.whamcloud.com/59871
Lustre-bug-id: https://jira.whamcloud.com/browse/LU-19121
Message-ID: <20250808080505.1307694-1-adilger@dilger.ca>
Signed-off-by: Theodore Ts'o <tytso@mit.edu>

lib/ext2fs/valid_blk.c
@@ -43,6 +43,7 @@ int ext2fs_inode_has_valid_blocks2(ext2_filsys fs, struct ext2_inode *inode)
 			/* With no EA block, we can rely on i_blocks */
 			if (inode->i_blocks == 0)
 				return 0;
+			return !ext2fs_is_fast_symlink(inode);
 		} else {
 			/* With an EA block, life gets more tricky */
 			if (inode->i_size >= EXT2_N_BLOCKS*4)

tests/f_ea_inode_fast_symlink/expect.1
@@ -1,0 +1,7 @@
+Pass 1: Checking inodes, blocks, and sizes
+Pass 2: Checking directory structure
+Pass 3: Checking directory connectivity
+Pass 4: Checking reference counts
+Pass 5: Checking group summary information
+test_filesys: 14/256 files (0.0% non-contiguous), 1147/8192 blocks
+Exit status is 0

tests/f_ea_inode_fast_symlink/expect.2
@@ -1,0 +1,7 @@
+Pass 1: Checking inodes, blocks, and sizes
+Pass 2: Checking directory structure
+Pass 3: Checking directory connectivity
+Pass 4: Checking reference counts
+Pass 5: Checking group summary information
+test_filesys: 14/256 files (0.0% non-contiguous), 1147/8192 blocks
+Exit status is 0

tests/f_ea_inode_fast_symlink/name
@@ -1,0 +1,1 @@
+fast symlink with ea_inode