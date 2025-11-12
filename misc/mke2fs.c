tytso/e2fsprogs

commit 06f50e001b7df448df523231bba26f1ca488f456

mke2fs: support multiple '-E' options
The '-E' option for specifying extended attributes can now be used
multiple times. The existing support for multiple attributes encoded
as comma-separated string is maintained for each '-E' option.

Prior to this change, if multiple '-E' options were specified, then
only the last one was used. Earlier ones were silently ignored.

Signed-off-by: Ralph Siemsen <ralph.siemsen@linaro.org>
Reviewed-by: Andreas Dilger <adilger@dilger.ca>
Message-ID: <20250910-mke2fs-small-fixes-v2-2-55c9842494e0@linaro.org>
Signed-off-by: Theodore Ts'o <tytso@mit.edu>

misc/mke2fs.c
@@ -1653,7 +1653,7 @@ static void PRS(int argc, char *argv[])
 	int		default_csum_seed = 0;
 	errcode_t	retval;
 	char *		oldpath = getenv("PATH");
-	char *		extended_opts = 0;
+	struct str_list extended_opts;
 	char *		fs_type = 0;
 	char *		usage_types = 0;
 	/*
@@ -1751,6 +1751,13 @@ profile_error:
 			journal_size = -1;
 	}
 
+	retval = init_list(&extended_opts);
+	if (retval) {
+		com_err(program_name, retval, "%s",
+			_("in malloc for extended_opts"));
+		exit(1);
+	}
+
 	while ((c = getopt (argc, argv,
 		    "b:cd:e:g:i:jl:m:no:qr:s:t:vC:DE:FG:I:J:KL:M:N:O:R:ST:U:Vz:")) != EOF) {
 		switch (c) {
@@ -1796,7 +1803,7 @@ profile_error:
 				_("'-R' is deprecated, use '-E' instead"));
 			/* fallthrough */
 		case 'E':
-			extended_opts = optarg;
+			push_string(&extended_opts, optarg);
 			break;
 		case 'e':
 			if (strcmp(optarg, "continue") == 0)
@@ -2615,8 +2622,9 @@ profile_error:
 			free(tmp);
 	}
 
-	if (extended_opts)
-		parse_extended_opts(&fs_param, extended_opts);
+	/* Get options from commandline */
+	for (cpp = extended_opts.list; *cpp; cpp++)
+		parse_extended_opts(&fs_param, *cpp);
 
 	if (fs_param.s_rev_level == EXT2_GOOD_OLD_REV) {
 		if (fs_features) {

misc/mke2fs.8.in
@@ -268,7 +268,9 @@ Cause a kernel panic.
 .TP
 .BI \-E " extended-options"
 Set extended options for the file system.  Extended options are comma
-separated, and may take an argument using the equals ('=') sign.  The
+separated, and may take an argument using the equals ('=') sign.  Multiple
+.B \-E
+options may also be used. The
 .B \-E
 option used to be
 .B \-R

commit 0b2752ce6276b383a5254cd766204f81f41b6631

mke2fs: fix memory leak which causes ASAN to complain
Fixes: 06f50e001b7d ("mke2fs: support multiple '-E' options")

Signed-off-by: Theodore Ts'o <tytso@mit.edu>

misc/mke2fs.c
@@ -1351,6 +1351,15 @@ static errcode_t init_list(struct str_list *sl)
 	return 0;
 }
 
+static void free_strlist(char **list)
+{
+	int	i;
+
+	for (i=0; list[i]; i++)
+		free(list[i]);
+	free(list);
+}
+
 static errcode_t push_string(struct str_list *sl, const char *str)
 {
 	char **new_list;
@@ -2673,6 +2682,7 @@ profile_error:
 	/* Get options from commandline */
 	for (cpp = extended_opts.list; *cpp; cpp++)
 		parse_extended_opts(&fs_param, *cpp);
+	free_strlist(extended_opts.list);
 
 	if (fs_param.s_rev_level == EXT2_GOOD_OLD_REV) {
 		if (fs_features) {
@@ -3764,8 +3774,6 @@ no_journal:
 	remove_error_table(&et_ext2_error_table);
 	remove_error_table(&et_prof_error_table);
 	profile_release(profile);
-	for (i=0; fs_types[i]; i++)
-		free(fs_types[i]);
-	free(fs_types);
+	free_strlist(fs_types);
 	return retval;
 }