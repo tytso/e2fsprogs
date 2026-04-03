5a65782e2286cd2c2d9ff32715f78eabcd853a1b
Theodore Ts'o <tytso@mit.edu>
2026-04-02 23:53:27 -0400

    fuse2fs: fix build failure on systems which don't define EUCLEAN
    MacOS doesn't have EUCLEAN, so we use EIO as the closest error code.
    But then we need to avoid a compile error caused by a duplicate case
    labels of EUCLEAN and EIO.
    
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- misc/fuse2fs.c
+++ misc/fuse2fs.c
@@ -5870,7 +5870,9 @@ static int __translate_error(ext2_filsys fs, ext2_ino_t ino, errcode_t err,
 #ifdef EILSEQ
 	case EILSEQ:
 #endif
+#if EUCLEAN != EIO
 	case EUCLEAN:
+#endif
 		/* these errnos usually denote corruption or persistence fail */
 		is_err = 1;
 		ret = -err;
