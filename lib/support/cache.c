447280ae5b5de1ea6fcc9a7f33505c8110e33ecc
Theodore Ts'o <tytso@mit.edu>
2026-05-04 04:13:18 -0400

    libsupport: drop xbitops.h and define fls() if necessary
    The new cache.c file doesn't actually need most of the functions in
    xbitops.h.  It only needs fls(), and on some systems, like MacOS and
    *BSD systems, fls() is defined already.  The other functions in
    xbitops.h are massively non-portable and break on non-Linux systems,
    especially MacOS.
    
    So define fls() if it's needed (on Linux), and drop xbitops.h.
    
    Fixes: 30b3c80ed6bc ("libsupport: add a cache")
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- lib/support/cache.c
+++ lib/support/cache.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <stdbool.h>
@@ -14,9 +15,9 @@
 #include <stdint.h>
 #include <errno.h>
 
+#include "config.h"
 #include "list.h"
 #include "cache.h"
-#include "xbitops.h"
 
 #undef CACHE_DEBUG
 /* #define CACHE_DEBUG 1 */
@@ -30,6 +31,36 @@
 # define ASSERT(x)		assert(x)
 #else
 # define ASSERT(x)		do { } while (0)
+#endif
+
+#ifndef HAVE_FLS
+static inline int fls(int x)
+{
+	int r = 32;
+
+	if (!x)
+		return 0;
+	if (!(x & 0xffff0000u)) {
+		x = (x & 0xffffu) << 16;
+		r -= 16;
+	}
+	if (!(x & 0xff000000u)) {
+		x = (x & 0xffffffu) << 8;
+		r -= 8;
+	}
+	if (!(x & 0xf0000000u)) {
+		x = (x & 0xfffffffu) << 4;
+		r -= 4;
+	}
+	if (!(x & 0xc0000000u)) {
+		x = (x & 0x3fffffffu) << 2;
+		r -= 2;
+	}
+	if (!(x & 0x80000000u)) {
+		r -= 1;
+	}
+	return r;
+}
 #endif
 
 static unsigned int cache_generic_bulkrelse(struct cache *, struct list_head *);
